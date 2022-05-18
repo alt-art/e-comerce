dde03992a4b2170600a87407054b87aac670c908
Pedro Mendes <pedromendescraft@gmail.com>
2022-05-18 15:28:25 -0300

    feat(prisma): seed database

---
+++ project/prisma/seed/index.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import axios from 'axios';
+
+type Rating = {
+  rate: number;
+  count: number;
+}
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: Rating;
+}
+
+(async () => {
+  const prisma = new PrismaClient();
+  const { data } = await axios.get('https://fakestoreapi.com/products');
+  const products = data as Product[];
+  const newProductsPromises = products.map((product) => prisma.product.create({
+    data: {
+      name: product.title,
+      description: product.description,
+      price: product.price,
+    },
+  }));
+  await Promise.all(newProductsPromises);
+})();
