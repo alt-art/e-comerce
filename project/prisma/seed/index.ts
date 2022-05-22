a3b34ebd226f20480f98225e7891bec8b0104c01
Pedro Mendes <pedromendescraft@gmail.com>
2022-05-22 19:13:36 -0300

    chore(prisma): seed user admin

--- project/prisma/seed/index.ts
+++ project/prisma/seed/index.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
+import { encrypt } from '../../src/utils/encryption';
 
 type Rating = {
   rate: number;
   count: number;
-}
+};
 
 type Product = {
   id: number;
@@ -14,10 +15,18 @@ type Product = {
   category: string;
   image: string;
   rating: Rating;
-}
+};
 
 (async () => {
   const prisma = new PrismaClient();
+  await prisma.user.create({
+    data: {
+      name: 'admin',
+      email: 'admin@test.com',
+      password: await encrypt('admin'),
+      role: 'ADMIN',
+    },
+  });
   const { data } = await axios.get('https://fakestoreapi.com/products');
   const products = data as Product[];
   const newProductsPromises = products.map((product) => prisma.product.create({
